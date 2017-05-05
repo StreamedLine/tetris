ea21c29a10f30854ae0c46eab556e72acd169d2e
David Lewitan <dovidlwtn@gmail.com>
2017-05-04 20:05:56 -0400

    put shapes in separate file. added shape 'cube'

---
+++ scripts/shapes.js
@@ -0,0 +1,124 @@
+
+(function createShapes(){
+	function Shape(name) {
+		var sd = 0, //distance from top
+			sx = 10; //distance from left side	
+		this.shapeMap = engine.shapes.shapes[name]; 
+		this.orientation = 0;
+		this.focused = true;
+		this.done = false;
+		this.sd = function(){return sd };
+		this.sx = function(){return sx };
+		this.calculatePostions = function() {
+			var guide = this.shapeMap[this.orientation];
+			return guide.map(function(pos){
+				return [sx + pos[0], sd + pos[1]];
+			});
+		}
+		this.gravity = function() {
+			var positions = this.calculatePostions(),
+			    guide = this.shapeMap[this.orientation];
+			guide = guide.filter(function(spot){return spot[2] === true });
+			if (guide.every(function(spot){return spot[1]+1+sd < game.screen.h && game.screen.xLines[spot[1]+1+sd][spot[0]+sx].chr === ' '})) {
+				positions.forEach(function(pos){game.screen.xLines[pos[1]][pos[0]].chr = ' ' });
+				sd += 1;
+			} else {
+				this.done = true;
+			}
+		};
+		this.reorient = function(val) {
+			var original = this.orientation;
+
+			this.calculatePostions().forEach(function(pos){
+				game.screen.xLines[pos[1]][pos[0]].chr = ' '
+			});
+
+			var ortn = this.orientation += val;
+			if (ortn < 0) {this.orientation = this.shapeMap.length-1}
+			if (ortn >= this.shapeMap.length) {this.orientation = 0}
+			var taken = this.calculatePostions().some(function(pos){
+				return pos[0] <= 0 || pos[0] >= game.screen.xLines[0].length ||
+					   pos[1] <= 0 || pos[1] >= game.screen.xLines.length ||
+					   game.screen.xLines[pos[1]][pos[0]].chr != ' '
+			});
+			if (taken) {
+				this.orientation = original;
+				this.calculatePostions().forEach(function(pos){
+					game.screen.xLines[pos[1]][pos[0]].chr = '#'
+				});
+				return false;
+			} else {
+				game.gameEngine.update_map();
+				game.insertFrameInDom(game.screenAsString());
+				return true;
+			}
+		};
+		this.moveLeft = function(val) {
+			var positions = this.calculatePostions(),
+				guide = this.shapeMap[this.orientation];
+			guide = guide.filter(function(spot){return spot[3].left === true });
+			if (guide.every(function(spot){return spot[0]-1+sx >= 0 && game.screen.xLines[spot[1]+sd][spot[0]-1+sx].chr === ' '})) {
+				positions.forEach(function(pos){game.screen.xLines[pos[1]][pos[0]].chr = ' ' });
+				sx -= 1;
+				game.gameEngine.update_map();
+				game.insertFrameInDom(game.screenAsString());
+			}
+
+		};
+		this.moveRight = function(val) {
+			var positions = this.calculatePostions(),
+				guide = this.shapeMap[this.orientation];
+			guide = guide.filter(function(spot){return spot[3].right === true });
+			if (guide.every(function(spot){return spot[0]+1+sx < game.screen.xLines[0].length && game.screen.xLines[spot[1]+sd][spot[0]+1+sx].chr === ' '})) {
+				positions.forEach(function(pos){game.screen.xLines[pos[1]][pos[0]].chr = ' ' });
+				sx += 1;
+				game.gameEngine.update_map();
+				game.insertFrameInDom(game.screenAsString());
+			}
+
+		};
+		this.keyboardPress = function(val) {
+			if (game.gameEngine.shapes.shapesArr.length == 0)
+				return
+			if (val == 37) {
+				this.moveLeft(-1);
+			} 
+			if (val == 39) {
+				this.moveRight(1)
+			}
+			if (val == 32) this.reorient(1)
+		};
+	window.shape = this
+	}//END Shape
+
+	var engine = game.gameEngine;
+
+	engine.shapes.shapeNames = ['line', 'plus', 'cube'];
+	
+	engine.shapes.shapes = {
+		line: [ 
+				[[0,0,false,{left: true, right: true}],[0,1,false,{left: true, right: true}],[0,2,true,{left: true, right: true}]] , 
+				[[-1,0,true,{left: true, right: false}],[0,0,true,{left: false, right: false}],[1,0,true,{left: false, right: true}]] 
+			],
+		plus: [  
+				[ [-1, 0, true, {left: true, right: false}] , [0, 0, false, {left: false, right: false}], [1, 0, true, {left: false, right: true}], [0, 1, true, {left: true, right: true}] ],
+				[ [0, 0, false, {left: true, right: true}] , [0, 1, false, {left: true, right: false}], [0, 2, true, {left: true, right: true}], [1, 1, true, {left: false, right: true}] ], 
+				[ [-1, 1, true, {left: true, right: false}] , [0, 1, true, {left: false, right: false}], [1, 1, true, {left: false, right: true}], [0, 0, false, {left: true, right: true}] ],
+				[ [0, 0, false, {left: true, right: true}] , [0, 1, false, {left: false, right: true}], [0, 2, true, {left: true, right: true}], [-1, 1, true, {left: true, right: false}] ],
+			],
+		// upstair: [
+		// 		[ [], [], [], [] ],
+		// 		[ [] ,[] ,[] ,[] ]
+		// 	],
+		// downstair: [
+		// 		[[], [], [], []],
+		// 		[[] ,[] ,[] ,[]]
+		// 	],
+		cube: [[[0,0, false, {left: true, right: false}], [1,0, false, {left: false, right: true}], [0,1, true, {left: true, right: false}], [1,1, true, {left: false, right: true}]]]
+	};
+
+	engine.shapes.create = function createShape() {
+		return new Shape( engine.shapes.shapeNames.randFromArr() );
+	};
+
+}());
